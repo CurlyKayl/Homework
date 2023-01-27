712a5ba4f652ee7f2218a43646035a21856d6f19
Curly_Kayl <endwarfor@gmail.com>
2023-01-27 14:51:18 +0300

    Exercise 25 completed.

---
+++ Homework_04/Exercise_25/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+int GetNumber(string message)
+{
+	int result = 0;
+
+	while (true)
+	{
+		Console.Write(message);
+
+		if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+		{
+			break;
+		}
+		else
+		{
+			Console.WriteLine("Ошибка! Введите число: ");
+		}
+	}
+
+	return result;
+}
+
+int number1 = GetNumber("Введите первое число: ");
+int number2 = GetNumber("Введите второе число: ");
+
+int GetDegree()
+{
+	int result = 1;
+
+	for (int i = 0; i < number2; i++)
+	{
+		result *= number1;
+	}
+	return result;
+}
+
+int deg = GetDegree();
+
+Console.Write($"Число {number1} в степени {number2} = {deg}");
+
---
+++ Homework_04/Exercise_27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+public static int Power(int a, int b) 
+{ 
+    int result = 1; 
+    for (int i = 0; i < b; i++) 
+    { 
+        result *= a; 
+    } 
+    return result; 
+}
