37e55922e5f516498691ea07f24b8cc4cfdac41a
Curly_Kayl <endwarfor@gmail.com>
2023-01-30 19:19:21 +0300

    Homework completed

--- Homework_05/Exercise_36/Program.cs
+++ Homework_05/Exercise_36/Program.cs
@@ -1,6 +1,67 @@
-﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
 Console.Clear();
 
+int GetNumber(string mes)
+{
+	int result = 0;
+	while (true)
+	{
+		Console.Write(mes);
+		if (int.TryParse(Console.ReadLine(), out result))
+		{
+			break;
+		}
+		else
+		{
+			Console.WriteLine("Ошибка!");
+		}
+	}
+	return result;
+}
+
+
+int[] GetArray(int mas)
+{
+	int[] arr = new int[mas];
+	Random rnd = new Random();
+
+	for (int i = 0; i < mas; i++)
+	{
+		arr[i] = rnd.Next(-1000, 1001);
+	}
+	return arr;
+}
+
+void PrintArray(int[] arr)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		Console.Write($"{arr[i]} ");
+	}
+	Console.WriteLine();
+}
+
+
+int SumNotEven(int[] array)
+{
+	int sum = 0;
+	int index = 0;
+	for (int i = 1; i < array.Length; i += 2)
+	{
+		sum += array[i];
+		index = array[i];
+		Console.WriteLine($"Нечетные элементы массива: {index}");
+	}
+	Console.WriteLine($"Сумма элементов с нечетными индексами массива: {sum}");
+
+	return sum;
+}
+
+int lengthNumber = GetNumber("Введите длину массива: ");
+int[] array = GetArray(lengthNumber);
+PrintArray(array);
+int sum = SumNotEven(array);
--- Homework_05/Exercise_38/Program.cs
+++ Homework_05/Exercise_38/Program.cs
@@ -1,5 +1,57 @@
-﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+﻿// Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
 Console.Clear();
 
+int GetLengthArray(string message)
+{
+	int result = 0;
+	while (true)
+	{
+		Console.Write(message);
+		if (int.TryParse(Console.ReadLine(), out result))
+		{
+			break;
+		}
+		else Console.WriteLine("Error!!!");
+	}
+	return result;
+}
+
+int[] GetArray(int mas)
+{
+	int[] arr = new int[mas];
+	Random rnd = new Random();
+	for (int i = 0; i < mas; i++)
+	{
+		arr[i] = rnd.Next(1, 101);
+	}
+	return arr;
+}
+
+void PrintArray(int[] arr)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		Console.Write($"{arr[i]} ");
+	}
+	Console.WriteLine();
+}
+
+int difference (int[] arr)
+{	
+	int result = 0;
+	int MaxArray = arr.Max();
+	int MinArray = arr.Min();
+	Console.WriteLine($"Максимальный элемент массива: {MaxArray}");
+	Console.WriteLine($"Минимальный элемент массива: {MinArray}");
+	result = MaxArray - MinArray;
+	return result;
+}
+
+int lengthNumber = GetLengthArray("Введите длину массива: ");
+int[] array = GetArray(lengthNumber);
+PrintArray(array);
+int diff = difference(array);
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива = {diff}");
