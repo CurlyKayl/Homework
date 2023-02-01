3f0e86085db0d3e33c993c749a62f3110ad7e129
Curly_Kayl <endwarfor@gmail.com>
2023-02-01 16:44:16 +0300

    Exercise 41

--- Homework_06/Exercise_41/Program.cs
+++ Homework_06/Exercise_41/Program.cs
@@ -1,7 +1,18 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь. 
 // Вводим через один Enter (разбиваем через Split()).
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
 Console.Clear();
 
+void GetNumbers()
+{
+	Console.WriteLine("Введите числа: ");
+	string nums = Console.ReadLine();
+	string[] words = nums.Split("");
+	Console.WriteLine(words);
+}
+
+GetNumbers();
+//Console.WriteLine(numbers);
