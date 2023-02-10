0d12def197f976177b38de67a3a681b90bef2faa
Curly_Kayl <endwarfor@gmail.com>
2023-02-10 10:26:03 +0300

    Exercise 52 completed.

--- Homework_07/Exercise_52/Program.cs
+++ Homework_07/Exercise_52/Program.cs
@@ -9,3 +9,71 @@
 
 Console.Clear();
 
+int GetNumbers(string message)
+{
+	int result = 0;
+	while (true)
+	{
+		Console.Write(message);
+
+		if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+		{
+			break;
+		}
+		else
+		{
+			Console.WriteLine("Ошибка. Не корректный ввод. Повторите ввод.");
+		}
+	}
+	return result;
+}
+
+int[,] InitMatrix(int rows, int columns)
+{
+	int[,] matrix = new int[rows, columns];
+	Random rnd = new Random();
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			matrix[i,j] = rnd.Next(1, 10);
+		}
+	}
+	return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{
+			Console.Write($"{matrix[i, j]} ");
+		}
+		Console.WriteLine();
+	}
+
+}
+
+void GetMediumColumns(int[,] matrix)
+{
+	double colmed = 0;
+	Console.Write("Среднее арифметическое каждого столбца матрицы: ");
+
+	for (int j = 0; j < matrix.GetLength(1); j++)
+	{	
+		colmed = 0;
+		for (int i = 0; i < matrix.GetLength(0); i++)
+		{
+			colmed += matrix[i, j];
+		}
+		Console.Write($"{colmed / matrix.GetLength(0)}; ");
+	}
+	Console.WriteLine();
+}
+
+int rows = GetNumbers("Введите количество строк в матрице: ");
+int columns = GetNumbers("Введите количество столбцов в матрице:");
+int[,] randmatrix = InitMatrix(rows, columns);
+PrintMatrix(randmatrix);
+GetMediumColumns(randmatrix);
