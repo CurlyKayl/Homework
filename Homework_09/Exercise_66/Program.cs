9a5308bbfafa0b614e690af7603f3e4743e0db8b
Curly_Kayl <endwarfor@gmail.com>
2023-02-12 19:06:05 +0300

    Exercise 66 completed.

--- Homework_09/Exercise_66/Program.cs
+++ Homework_09/Exercise_66/Program.cs
@@ -5,3 +5,36 @@
 
 Console.Clear();
 
+int GetNumber(string message)
+{
+	int result = 0;
+	while (true)
+	{
+		Console.Write(message);
+
+		if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+		{
+			break;
+		}
+			else
+		{
+		Console.WriteLine("Ошибка. Не корректный ввод. Повторите ввод.");
+		}
+	}	
+	return result;
+}
+
+int SumOfNumericRange(int m, int n)
+{
+    if (m < n)
+        return m + SumOfNumericRange(m + 1, n);
+    else
+        return n;
+}
+
+int m = GetNumber("Введите первое число: ");
+int n = GetNumber("Введите второе число: ");
+if (m > n)
+    Console.WriteLine("Первое число должно быть меньше второго.");
+else
+    Console.WriteLine(SumOfNumericRange(m, n));
