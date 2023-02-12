84390bb5f53ba69c414873bfc3aad7a0cccd8804
Curly_Kayl <endwarfor@gmail.com>
2023-02-12 19:09:17 +0300

    Exercise 68 completed.

--- Homework_09/Exercise_68/Program.cs
+++ Homework_09/Exercise_68/Program.cs
@@ -5,3 +5,35 @@
 
 Console.Clear();
 
+int GetNumber(string message)
+{
+	int result = 0;
+	while (true)
+	{
+		Console.Write(message);
+
+		if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+		{
+			break;
+		}
+			else
+		{
+		Console.WriteLine("Ошибка. Не корректный ввод. Повторите ввод.");
+		}
+	}	
+	return result;
+}
+
+int Accerman(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else if (n > 0 && m == 0)
+        return Accerman(n - 1, 1);
+    else
+        return Accerman(n - 1, Accerman(n, m - 1));
+}
+
+int n = GetNumber("Введите первый аргумент: ");
+int m = GetNumber("Введите второй аргумент: ");
+Console.WriteLine($"A({n},{m}) = {Accerman(n, m)}");
